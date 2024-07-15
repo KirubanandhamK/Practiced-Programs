8b140eb3e3e54536f7db8b3ab7eeeba658f48e9c
KirubanandhamK <99739755+KirubanandhamK@users.noreply.github.com>
2024-07-15 20:17:57 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in= new Scanner(System.in);
+		System.out.println("enter two numbers");
+		int num1= in.nextInt();
+		int num2= in.nextInt();
+		System.out.println(num1+num2);
+	}
+}
